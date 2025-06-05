01214350c039a3e52ca8d732c9539a211a0a517d
Netcafe96 <myblack532@gmail.com>
2025-06-05 07:40:36 -0400

    Update scene_detection.py

--- app/scene_detection.py
+++ app/scene_detection.py
@@ -1,1 +1,12 @@
+from scenedetect import VideoManager, SceneManager
+from scenedetect.detectors import ContentDetector
+
+def detect_scenes(video_path):
+    video_manager = VideoManager([video_path])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(ContentDetector())
+    video_manager.set_downscale_factor()
+    video_manager.start()
+    scene_manager.detect_scenes(frame_source=video_manager)
+    return scene_manager.get_scene_list()
 
