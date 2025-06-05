c7836029849c459f01fed9e04709e7efb996f9a5
Netcafe96 <myblack532@gmail.com>
2025-06-05 07:39:57 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,10 @@
+import os
+import tempfile
+
+def save_uploaded_file(uploaded_file):
+    temp_dir = tempfile.mkdtemp()
+    path = os.path.join(temp_dir, uploaded_file.name)
+    with open(path, "wb") as f:
+        f.write(uploaded_file.read())
+    return path
 
